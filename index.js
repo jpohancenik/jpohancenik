df39b74c0505ee23afc8c5cd70b0c72be5ecef21
jpohancenik <jakub.pohancenik@cleverlance.com>
2023-02-02 12:59:31 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const sass = require('node-sass');
+const fs = require('fs')
+const inlineCss = require('inline-css');
+
+const htmlContent = fs.readFileSync('./src/template.html').toString()
+
+sass.render({
+  file: './src/styles.scss',
+}, (error, result) => {
+  error && console.error('SASS ERROR', error)
+  !error && inlineCss(htmlContent.replace('[styles]', result.css), {
+    url: '/.dist/style.css',
+    preserveMediaQueries: true,
+    removeHtmlSelectors: true,
+  })
+    .then((result) => {
+      fs.writeFileSync('./dist/index.html', result.toString());
+    })
+    .catch((e) => console.log(e));
+});
